b803be585de22a81da246dd59173d217ff89d5cd
tonyz0909 <tonyz0909@gmail.com>
2021-05-16 21:03:19 -0400

    redesigned card and popup UI

--- src/components/OutlinedCard.js
+++ src/components/OutlinedCard.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import Link from '@material-ui/core/Link';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import CancelIcon from '@material-ui/icons/Cancel';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Trailer from './Trailer';
+import Trailer, { SimpleDialog } from './Trailer';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 
 const useStyles = makeStyles({
   root: {
@@ -29,13 +37,32 @@ let getColor = (rating, isIMDB) => {
   if (isIMDB == true && rating == 10 || rating == 100) {
     return 'blue';
   } else if (isIMDB == true && rating > 7.5 || rating > 85) {
-    return 'green';
-  } else if (isIMDB == true && rating > 6 || rating > 75) {
+    return '#31b830'; // '#77DD76';
+  } else if (isIMDB == true && rating > 6 || rating > 70) {
     return 'orange';
   } else {
-    return 'red';
+    return '#FF6962'; // '#db3d00';
   }
 }
+
+const options = [
+  'None',
+  'Atria',
+  'Callisto',
+  'Dione',
+  'Ganymede',
+  'Hangouts Call',
+  'Luna',
+  'Oberon',
+  'Phobos',
+  'Pyxis',
+  'Sedna',
+  'Titania',
+  'Triton',
+  'Umbriel',
+];
+
+const ITEM_HEIGHT = 48;
 
 export default function OutlinedCard(props) {
   const classes = useStyles();
@@ -45,59 +72,122 @@ export default function OutlinedCard(props) {
   const imdbColor = getColor(props.movieData['imdbRating'], true);
   const rtColor = getColor(props.movieData['rottenTomatoesRating'], false);
 
-  // const ratings = getRatings();
+  // menu config
+  // const [anchorEl, setAnchorEl] = useState(null);
+  // const open = Boolean(anchorEl);
 
-  // let getRatings = () => {
-  //   []
-  // }
+  // const handleClick = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  // };
+
+  const [showClose, setClose] = useState('none');
+
+  // dialog handler
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
 
   return (
-    <Card className={classes.root} variant="outlined" style={{maxWidth:"450px", margin:'auto'}}>
-      <CardContent>
+    <Card className={classes.root} variant="outlined" style={{ maxWidth: "450px", margin: 'auto' }}>
+      <CardContent
+        style={{ position: 'relative' }}
+        onMouseEnter={() => setClose('block')}
+        onMouseLeave={() => setClose('none')}>
+        {!props.useTrending &&
+          <IconButton
+            aria-label="more"
+            aria-controls="long-menu"
+            aria-haspopup="true"
+            style={{ position: 'absolute', top: -5, right: -5, display: showClose }}
+            onClick={() => props.deleteMovie(props.movieData['title'])}
+          >
+            <CancelIcon />
+          </IconButton>}
         <Grid container spacing={4}>
           <Grid item xs={8}>
             {/* <Typography className={classes.title} color="textSecondary" gutterBottom>
           Word of the Day
         </Typography> */}
-            <Typography variant="h6" component="h2">
-              {props.movieData['title']}
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              {Object.values(props.movieData['metadata']).join(" ‧ ")}
-            </Typography>
-            <Grid container spacing={2} style={{marginTop: '1.5em'}}>
-              <Grid item xs={4} style={{textAlign:'center'}}>
-                <span style={{color:guColor}}>
-                  {props.movieData['googleUsersPercent']}%<br/>
+            <Link href="#" onClick={(e) => { e.preventDefault(); handleClickOpen(); }} color="inherit">
+              <Typography variant="h6" component="h2">
+                {props.movieData['title']}
+                {/* <IconButton
+                  aria-label="more"
+                  aria-controls="long-menu"
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                >
+                  <ArrowForwardIosIcon fontSize="small" />
+                </IconButton>
+                <Menu
+                  id="long-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={open}
+                  onClose={handleClose}
+                  PaperProps={{
+                    style: {
+                      maxHeight: ITEM_HEIGHT * 4.5,
+                      width: '20ch',
+                    },
+                  }}
+                >
+                  {options.map((option) => (
+                    <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Menu> */}
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                {Object.values(props.movieData['metadata']).join(" ‧ ")}
+              </Typography>
+            </Link>
+            <Grid container spacing={2} style={{ marginTop: '1.5em' }}>
+              <Grid item xs={4} style={{ textAlign: 'center' }}>
+                <span style={{ color: guColor }}>
+                  {props.movieData['googleUsersPercent']}%<br />
                 Google Users
                 </span>
               </Grid>
-              <Grid item xs={4} style={{textAlign:'center'}}>
-                <span style={{color:imdbColor}}>
-                  {props.movieData['imdbRating']}/10<br/>
+              <Grid item xs={4} style={{ textAlign: 'center' }}>
+                <span style={{ color: imdbColor }}>
+                  {props.movieData['imdbRating']}/10<br />
                 IMDb
                 </span>
               </Grid>
-              <Grid item xs={4} style={{textAlign:'center'}}>
-                <span style={{color:rtColor}}>
-                  {props.movieData['rottenTomatoesRating']}%<br/>
+              <Grid item xs={4} style={{ textAlign: 'center' }}>
+                <span style={{ color: rtColor }}>
+                  {props.movieData['rottenTomatoesRating']}%<br />
                 Rotten Tomatoes
                 </span>
               </Grid>
             </Grid>
-            {/* <Typography variant="body1" component="p">
-              {props.movieData['googleUsersRating']}
-            </Typography>
-            <Typography variant="body1" component="p">
-              IMBD rating: {props.movieData['imdbRating']}/10
-            </Typography>
-            <Typography variant="body1" component="p">
-              Rotten Tomatoes score: {props.movieData['rottenTomatoesRating']}%
-            </Typography> */}
-            {/* <Trailer url={props.movieData['trailerUrl']}/> */}
           </Grid>
-          <Grid item xs={4}>
-            <Trailer imgSrc={imgSrc} url={props.movieData['trailerUrl']} />
+          <Grid item xs={4} >
+            <div>
+              <img
+                src={imgSrc}
+                alt="new"
+                width={67 * 1.6}
+                height={100 * 1.6}
+                style={{ height: '100%' }}
+              />
+              {/* <IconButton color="primary" aria-label="upload picture" component="span" style={{ position: 'absolute', marginTop: '50px', marginLeft: '-80px' }} onClick={handleClickOpen}>
+                <PlayCircleOutlineIcon fontSize="large" />
+              </IconButton> */}
+              <SimpleDialog open={open} onClose={handleClose} url={props.movieData['trailerUrl']} movieData={props.movieData} />
+            </div>
           </Grid>
         </Grid>
 
--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Avatar from '@material-ui/core/Avatar';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -9,6 +10,9 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 import PersonIcon from '@material-ui/icons/Person';
 import AddIcon from '@material-ui/icons/Add';
 import Typography from '@material-ui/core/Typography';
@@ -22,9 +26,29 @@ const useStyles = makeStyles({
         backgroundColor: blue[100],
         color: blue[600],
     },
+    root: {
+        maxWidth: 853,
+        overflowY: 'scroll'
+    },
 });
 
-function SimpleDialog(props) {
+let getColor = (rating, isIMDB) => {
+    if (isIMDB == true && rating == 10 || rating == 100) {
+        return 'blue';
+    } else if (isIMDB == true && rating > 7.5 || rating > 85) {
+        return '#31b830';
+    } else if (isIMDB == true && rating > 6 || rating > 70) {
+        return 'orange';
+    } else {
+        return '#FF6962';
+    }
+}
+
+export function SimpleDialog(props) {
+    const guColor = getColor(props.movieData['googleUsersPercent'], false);
+    const imdbColor = getColor(props.movieData['imdbRating'], true);
+    const rtColor = getColor(props.movieData['rottenTomatoesRating'], false);
+
     const classes = useStyles();
     const { onClose, selectedValue, open } = props;
 
@@ -36,12 +60,81 @@ function SimpleDialog(props) {
         onClose(value);
     };
 
+    let getSummary = async () => {
+        if (open == true) {
+            let url = `https://api.themoviedb.org/3/search/movie?api_key=210fc31dd8bed65f0aaba2bf322a7627&language=en-US&query=${props.movieData['title']}&page=1`
+            let response = await fetch(url);
+            let movieInfo = await response.json();
+            console.log(movieInfo);
+            if (movieInfo.results[0]) setSummary(movieInfo.results[0].overview);
+        }
+    }
+
+    useEffect(getSummary, [open]);
+
+    const [summary, setSummary] = useState(null);
+
     return (
         <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}
             maxWidth="lg">
             <iframe width="853" height="480" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
                 src={props.url}>
             </iframe>
+            <Card className={classes.root}>
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                        Summary
+                    </Typography>
+                    <hr />
+                    <Typography variant="body1" component="p" color="textSecondary">
+                        {summary}
+                    </Typography>
+                    <Container maxWidth="sm">
+                        <Grid container spacing={2} style={{ marginTop: '1em', marginBottom: '1em' }}>
+                            <Grid item xs={4} style={{ textAlign: 'center' }}>
+                                <span style={{ color: guColor }}>
+                                    {props.movieData['googleUsersPercent']}%<br />
+                            Google Users
+                            </span>
+                            </Grid>
+                            <Grid item xs={4} style={{ textAlign: 'center' }}>
+                                <span style={{ color: imdbColor }}>
+                                    {props.movieData['imdbRating']}/10<br />
+                            IMDb
+                            </span>
+                            </Grid>
+                            <Grid item xs={4} style={{ textAlign: 'center' }}>
+                                <span style={{ color: rtColor }}>
+                                    {props.movieData['rottenTomatoesRating']}%<br />
+                            Rotten Tomatoes
+                            </span>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                    <hr/>
+                    <Typography variant="h6" component="h2">
+                        Other Information
+                    </Typography>
+                    <Grid container spacing={2} style={{ marginTop: '1em', marginBottom: '1em' }}>
+                        <Grid item xs={6}>
+                            <Typography className={classes.pos} color="textSecondary">
+                                Rating: {props.movieData['metadata']['rating']}
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                Release Date: {props.movieData['metadata']['year']}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography className={classes.pos} color="textSecondary">
+                                Genre: {props.movieData['metadata']['genre']}
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                Duration: {props.movieData['metadata']['duration']}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
         </Dialog>
     );
 }
@@ -54,7 +147,6 @@ SimpleDialog.propTypes = {
 
 export default function Trailer(props) {
     const [open, setOpen] = React.useState(false);
-    const [selectedValue, setSelectedValue] = React.useState(emails[1]);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -62,29 +154,21 @@ export default function Trailer(props) {
 
     const handleClose = (value) => {
         setOpen(false);
-        setSelectedValue(value);
     };
 
     return (
         <div>
-            {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Open simple dialog
-      </Button> */}
             <img
                 src={props.imgSrc}
                 alt="new"
                 width={67 * 1.6}
                 height={100 * 1.6}
                 style={{ height: '100%' }}
-            // onMouseOver={handleClickOpen}
-            // style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
             />
-            {/* <Button color="secondary" style={{ position: 'absolute', marginTop: '-100px', marginLeft: '25px' }}>Test</Button> */}
-            {/* <IconButton color="primary" aria-label="upload picture" component="span" style={{ position: 'absolute', marginTop: '-110px', marginLeft: '26px' }} onClick={handleClickOpen}> */}
             <IconButton color="primary" aria-label="upload picture" component="span" style={{ position: 'absolute', marginTop: '50px', marginLeft: '-80px' }} onClick={handleClickOpen}>
                 <PlayCircleOutlineIcon fontSize="large" />
             </IconButton>
-            <SimpleDialog selectedValue={selectedValue} open={open} onClose={handleClose} url={props.url} />
+            <SimpleDialog open={open} onClose={handleClose} url={props.url} movieData={props.movieData} />
         </div>
     );
 }
